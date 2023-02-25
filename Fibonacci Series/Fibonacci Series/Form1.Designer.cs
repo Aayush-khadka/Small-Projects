c91d740bab58fae5348579356ea63d6882823b68
Aayush <Khadkaaayush90@gmail.com>
2023-02-25 11:33:12 +0545

    finalized

--- Fibonacci Series/Fibonacci Series/Form1.Designer.cs
+++ Fibonacci Series/Fibonacci Series/Form1.Designer.cs
@@ -28,17 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
             this.btn_generate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txt_num = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txt_show = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_generate
             // 
-            this.btn_generate.Location = new System.Drawing.Point(209, 141);
+            this.btn_generate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn_generate.Location = new System.Drawing.Point(185, 182);
             this.btn_generate.Name = "btn_generate";
             this.btn_generate.Size = new System.Drawing.Size(85, 27);
             this.btn_generate.TabIndex = 0;
@@ -48,25 +52,28 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 75);
+            this.label1.Location = new System.Drawing.Point(123, 107);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Enter your Number :";
+            this.label1.Text = "Count :";
             // 
             // txt_num
             // 
-            this.txt_num.Location = new System.Drawing.Point(194, 72);
+            this.txt_num.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_num.Location = new System.Drawing.Point(170, 104);
             this.txt_num.Name = "txt_num";
             this.txt_num.Size = new System.Drawing.Size(100, 20);
             this.txt_num.TabIndex = 2;
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Minecraftia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(67, 23);
+            this.label2.Location = new System.Drawing.Point(59, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(245, 37);
             this.label2.TabIndex = 3;
@@ -74,8 +81,9 @@
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(87, 118);
+            this.label3.Location = new System.Drawing.Point(63, 150);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 13);
             this.label3.TabIndex = 4;
@@ -83,25 +91,41 @@
             // 
             // txt_show
             // 
-            this.txt_show.Location = new System.Drawing.Point(194, 115);
+            this.txt_show.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_show.Location = new System.Drawing.Point(170, 147);
             this.txt_show.Name = "txt_show";
             this.txt_show.Size = new System.Drawing.Size(100, 20);
             this.txt_show.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(192)))), ((int)(((byte)(139)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, -6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(372, 80);
+            this.panel1.TabIndex = 6;
             // 
             // form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(372, 268);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.txt_show);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.txt_num);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_generate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fibonacci Series";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +139,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txt_show;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
